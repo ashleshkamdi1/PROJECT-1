7ea4e1c647e988bcdfcb15aa060ff9c390753f1b
ashleshkamdi1 <79220532+ashleshkamdi1@users.noreply.github.com>
2021-02-17 23:50:00 +0530

    Create SIZES OF DATATYPES.cpp

---
+++ SIZES OF DATATYPES.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char a;
+    int b;
+    float c;
+    double d;
+      cout<<sizeof a;
+      cout<<"\n"<<sizeof b;
+      cout<<"\n"<<sizeof c;
+      cout<<"\n"<<sizeof d;
+   return 0;
+}
